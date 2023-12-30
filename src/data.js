eedf2f3107e24a373c8050ebf31db28c119d5f07
Lux <luxprajapati81@gmail.com>
2023-12-30 16:16:09 +0530

    Testimonial Slider

---
+++ src/data.js
@@ -0,0 +1,44 @@
+const reviews = [
+  {
+    id: 1,
+    name: "Lux Prajapati",
+    job: "Software Engineer",
+    image:
+      "https://media.licdn.com/dms/image/D4D03AQGO8VsWeBwQZQ/profile-displayphoto-shrink_400_400/0/1694860980156?e=1709164800&v=beta&t=92-36v4V7t_bVMdstIm-iAKCYfdMyPvjLM8gizqhPY0",
+    text: "I have had the pleasure of working with this team on several projects, and I am consistently impressed with their technical expertise and ability to deliver quality solutions on time and within budget. They are a true partner and an asset to any project.",
+  },
+  {
+    id: 2,
+    name: "Aryan Gajjar",
+    job: "Full Stack Developer",
+    image:
+      "https://media.licdn.com/dms/image/D4D03AQHIUwskkzciCg/profile-displayphoto-shrink_100_100/0/1701237685613?e=1709164800&v=beta&t=7BYqtQ8C49uJYUI-lNjUKMtnAMtZtkq7kfEvwoWvpWM",
+    text: "I have been working with this company for several years now, and I have always been impressed with their creativity and attention to detail. They are true professionals who take pride in their work and always go above and beyond to deliver exceptional results.",
+  },
+  {
+    id: 3,
+    name: "Nishant Chahar",
+    job: "Marketing Manager",
+    image:
+      "https://media.licdn.com/dms/image/D5603AQGyXMr5RXkkwQ/profile-displayphoto-shrink_400_400/0/1675256846656?e=1709164800&v=beta&t=rei4KK7sZCTfW62b29eahCXsfct5pzeo51XcUZ-Iyko",
+    text: "I am thrilled with the results of our recent marketing campaign, and it wouldn't have been possible without the hard work and dedication of the entire team. Thank you for your exceptional work!",
+  },
+  {
+    id: 4,
+    name: "Tanay Pratap",
+    job: "Developer & Founder",
+    image:
+      "https://media.licdn.com/dms/image/C5103AQGVpPvlt0HvkA/profile-displayphoto-shrink_400_400/0/1562407623814?e=1709164800&v=beta&t=WKAJL7p_h8BlhpF9rkq-0QFvB0deewGQy3l0chgtvaA",
+    text: "I have worked with many content creators over the years, but none have impressed me as much as this team. They have a knack for crafting compelling and engaging content that resonates with our audience and drives real results. I highly recommend them!",
+  },
+  {
+    id: 5,
+    name: "Gaurav Malvi",
+    job: "Data Analyst",
+    image:
+      "https://media.licdn.com/dms/image/D5603AQFe-i1-_G0eWA/profile-displayphoto-shrink_100_100/0/1690551881311?e=1709164800&v=beta&t=7sP_777-ZnPeUd3Ye3TtltbVShw3Cnh6afmNV63efL0",
+    text: "I had the opportunity to work with this team on a data analysis project, and I was impressed with their attention to detail and their ability to work with complex datasets. They provided valuable insights that helped us make informed business decisions. It was a pleasure working with them.",
+  },
+];
+
+export default reviews;
